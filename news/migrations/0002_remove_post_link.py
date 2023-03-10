76f2681ebbedf99363f00aebc3e6696d1be4a652
Юрий <124659396+j41kkk@users.noreply.github.com>
2023-03-10 08:51:44 +0300

    Create 0002_remove_post_link.py

---
+++ news/migrations/0002_remove_post_link.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('news', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='post',
+            name='link',
+        ),
+    ]
